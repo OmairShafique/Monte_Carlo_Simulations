e0c8469764903752e77ef59e09e4a31ca65480fc
CrazyScholar98 <omair.shafiq1998@gmail.com>
2023-12-31 15:32:01 +0500

    chang

--- Monte_Carlo_Simulations/MainWindow.xaml.cs
+++ Monte_Carlo_Simulations/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MontDep;
 
 namespace Monte_Carlo_Simulations
 {
@@ -20,9 +21,76 @@ namespace Monte_Carlo_Simulations
     /// </summary>
     public partial class MainWindow : Window
     {
+        // generate a list of shapes
+        public static List<MontDep.Shapes> shapes = new List<MontDep.Shapes>();
+        // generate a random number generator
+        public static Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Engine();
+        }
+
+        public static void Engine()
+        {
+            try
+            {
+                #region Creating the Shapes
+
+                double[] Origin_Boundries = new double[4];
+                //Origin_Boundries[0] = 
+
+                // generate 20 random shapes
+                for (int i = 0; i < 20; i++)
+                {
+                    // generate a random number of sides
+                    int n_of_sides = random.Next(3, 9);
+
+                    // generate a random number of sides
+                    double[] sides = new double[n_of_sides];
+
+                    // generate random lengths for the sides
+                    for (int j = 0; j < n_of_sides; j++)
+                    {
+                        sides[j] = random.Next(1, 100);
+                    }
+
+                    // generate a random origin
+                    double[] origin = new double[2];
+
+                    // generate random coordinates for the origin
+                    origin[0] = random.Next(1, 100);
+                    origin[1] = random.Next(1, 100);
+
+                    // generate random angles for the shape
+                    double[] angles = new double[n_of_sides];
+
+                    // generate random angles for the shape
+                    for (int j = 0; j < n_of_sides - 1; j++)
+                    {
+                        angles[j] = random.Next(0, 180);
+                    }
+
+                    // generate the last angle
+                    angles[n_of_sides - 1] = 360 - angles.Sum();
+
+                    // add the shape to the list of shapes
+                    shapes.Add(new MontDep.Shapes(i, n_of_sides, origin, sides, angles));
+                }
+                #endregion
+
+                #region Posting to the Canvas
+
+                
+
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
